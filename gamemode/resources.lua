0f46e6ae41c5069dcf1d6631518b38a3c4c3de5f
Borgli <rune.borgli@gmail.com>
2015-05-29 15:01:40 +0200

    Added flashlight to res. Still needs implementation

--- gamemode/resources.lua
+++ gamemode/resources.lua
@@ -2,4 +2,7 @@
 resource.AddWorkshop("156921981")
 resource.AddWorkshop("400762901")
 resource.AddWorkshop("448581592")
+resource.AddWorkshop("112806637")
+resource.AddWorkshop("144982052")
+resource.AddWorkshop("244540803")
 resource.AddFile("models/player/bobert/joker.mdl")
