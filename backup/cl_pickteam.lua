a0d6ae8299362bbde034dc9516b16b72726fab1e
arildlil <arildlil@student.matnat.uio.no>
2016-02-12 12:41:38 +0100

    Modified file structure.

---
+++ backup/cl_pickteam.lua
@@ -0,0 +1,206 @@
+include('shared.lua')
+
+--[[---------------------------------------------------------
+   Name: gamemode:ShowTeam()
+   Desc:
+-----------------------------------------------------------]]
+
+function GM:ShowTeam(ply)
+	
+	if (IsValid(self.TeamSelectFrame)) then return end
+
+	self.TeamSelectFrame = vgui.Create("DFrame")
+	self.TeamSelectFrame:SetTitle("Select Monster or Survivor")
+
+	-- Right side of the spawn menu
+	local RightPanel = vgui.Create("DPanel", self.TeamSelectFrame)
+	RightPanel:SetPos(10, 30)
+	RightPanel:SetSize(ScrW() /4 , ScrH() - 50)
+	--RightPanel:SetBackgroundColor(Color(100,0,0,255))
+
+	local MonsterButton = vgui.Create("DButton", RightPanel)
+	local w, h = RightPanel:GetSize()
+	MonsterButton:SetPos(0, 0)
+	MonsterButton:SetSize(w, (h/2)*(1/3))
+	MonsterButton:SetText("Monsters")
+	
+
+	local SurvivorButton = vgui.Create("DButton", RightPanel) 
+	local w, h = RightPanel:GetSize()
+	SurvivorButton:SetPos(0, (h/2)*(1/3))
+	SurvivorButton:SetSize(w, (h/2)*(1/3))
+	SurvivorButton:SetText("Survivors")
+
+	local SpectatorButton = vgui.Create("DButton",RightPanel)
+	local w, h = RightPanel:GetSize()
+	SpectatorButton:SetPos(0, ((h/2)*(1/3))*2)
+	SpectatorButton:SetSize(w, (h/2)*(1/3))
+	SpectatorButton:SetText("Spectators")
+	function SpectatorButton.DoClick() self:HideTeam() RunConsoleCommand("changeteam", TEAM_SPECTATOR) end
+
+	local SelectPanel = vgui.Create("DScrollPanel", RightPanel)
+	local w, h = RightPanel:GetSize()
+	--TextPanel:SetPos(h/2, w/2)
+	SelectPanel:SetSize(w,h/2)
+	SelectPanel:SetBackgroundColor(Color(100,0,0,255))
+	SelectPanel:AlignBottom(0)
+	
+	--self:ClickMonsters(SelectPanel);
+
+	function MonsterButton.DoClick() self:ClickMonsters(SelectPanel) end
+	function SurvivorButton.DoClick() self:ClickSurvivors(SelectPanel) end
+
+--[[
+	local Description = vgui.Create("DLabel", TextPanel)
+	Description:SetSize(TextPanel:GetSize())
+	Description:SetText("HEI")
+]]--
+
+	self.ModelSelect = vgui.Create("DModelPanel", self.TeamSelectFrame)
+	self.ModelSelect:SetPos(ScrW() /4, 30)
+	self.ModelSelect:SetSize((ScrW()/4) * 3, ScrH() - 50)
+	self.ModelSelect:SetLookAt(Vector(0,0,60))
+	self.ModelSelect:SetCamPos(Vector(30,0,65))
+
+	
+
+
+	self.TeamSelectFrame:SetSize(ScrW(), ScrH())
+	self.TeamSelectFrame:Center()
+	self.TeamSelectFrame:MakePopup()
+	self.TeamSelectFrame:SetKeyboardInputEnabled(false)
+
+end
+
+
+--[[--------------------------------------------------------
+function GM:ShowTeam()
+
+	if (IsValid(self.TeamSelectFrame)) then return end
+
+	-- Simple team selection box
+	self.TeamSelectFrame = vgui.Create("DFrame")
+	self.TeamSelectFrame:SetTitle("Pick Team")
+
+	local AllTeams = team.GetAllTeams()
+	local y = 30
+	for ID, TeamInfo in pairs (AllTeams) do
+
+		if (ID ~= TEAM_CONNECTING and ID ~= TEAM_UNASSIGNED) then
+
+			local Team = vgui.Create("DButton", self.TeamSelectFrame)
+			function Team.DoClick() self:HideTeam() RunConsoleCommand("changeteam", ID) end
+			Team:SetPos(10, y)
+			Team:SetSize(130, 20)
+			Team:SetText(TeamInfo.Name)
+
+			if (IsValid(LocalPlayer()) and LocalPlayer():Team() == ID) then
+				Team:SetDisabled(true)
+			end
+
+			y = y + 30
+
+		end
+
+	end
+
+	if (GAMEMODE.AllowAutoTeam) then
+
+		local Team = vgui.Create("DButton", self.TeamSelectFrame)
+		function Team.DoClick() self:HideTeam() RunConsoleCommand("autoteam") end
+		Team:SetPos(10, y)
+		Team:SetSize(130, 20)
+		Team:SetText("Auto")
+		y = y + 30
+
+	end
+
+	self.TeamSelectFrame:SetSize(150, y)
+	self.TeamSelectFrame:Center()
+	self.TeamSelectFrame:MakePopup()
+	self.TeamSelectFrame:SetKeyboardInputEnabled(false)
+
+end
+-----------------------------------------------------]]
+
+
+--[[---------------------------------------------------------
+   Name: gamemode:HideTeam()
+   Desc:
+-----------------------------------------------------------]]
+
+function GM:ClickSurvivors(SelectPanel)
+	local SurvivorModels = {
+		"models/player/kleiner.mdl",
+		"models/player/alyx.mdl",
+		"models/player/barney.mdl"
+	}
+	self:DisplayModelList(SelectPanel, SurvivorModels, TEAM_SURVIVORS, ply)
+end
+
+function GM:ClickMonsters(SelectPanel)
+	local MonsterModels = {
+		"models/player/bobert/AOJoker.mdl",
+		"models/player/breen.mdl"
+	}
+	self:DisplayModelList(SelectPanel, MonsterModels, TEAM_MONSTERS, ply)
+end
+
+function GM:DisplayModelList(SelectPanel, models, team)
+	local x = 0
+	local y = 0
+	
+	local w, h = SelectPanel:GetSize()
+	SelectPanel:Clear()
+	for i = 1, #models do
+		local SelectModel = vgui.Create("DModelPanel",SelectPanel, "select")
+		--SelectModel:SetPos(x/3 , h/3 * (y % 3))
+		SelectModel:SetPos(w/3 * x, h/3 * y)
+		SelectModel:SetSize(w/3,h/3)
+		if x == 2 then
+			x = 0
+			y = y + 1
+		else
+			x = x + 1
+		end
+		SelectModel:SetLookAt(Vector(0,0,60))
+		SelectModel:SetCamPos(Vector(30,0,65))
+		SelectModel:SetModel(models[i])
+		SelectModel:GetEntity():SetEyeTarget(SelectModel:GetCamPos() - Vector(0,0,5))
+
+		function SelectModel:LayoutEntity(entity) return end
+		function SelectModel.DoClick()
+			RunConsoleCommand("changeteam", team)
+			player_manager.SetPlayerClass(LocalPlayer(), "player_" .. (team == TEAM_MONSTERS and "monster" or "survivor"))
+			--player_manager.RunClass(LocalPlayer(),"SetModel",models[i])
+			self:HideTeam()
+		end
+	end
+end
+
+function GM:DisplaySelectedModel(model)
+	if (not IsValid(self.ModelSelect)) then
+		self.ModelSelect = vgui.Create("DModelPanel", self.TeamSelectFrame)
+		self.ModelSelect:SetPos(ScrW() /4, 30)
+		self.ModelSelect:SetSize((ScrW()/4) * 3, ScrH() - 50)
+		self.ModelSelect:SetLookAt(Vector(0,0,60))
+		self.ModelSelect:SetCamPos(Vector(30,0,65))
+	end
+
+	if (self.ModelSelect:GetModel() ~= model) then
+		self.ModelSelect:SetModel(model)
+		self.ModelSelect:GetEntity():SetEyeTarget(self.ModelSelect:GetCamPos() - Vector(0,0,5))
+
+		function self.ModelSelect:LayoutEntity(entity) return end
+	end
+
+end
+
+function GM:HideTeam()
+
+	if (IsValid(self.TeamSelectFrame)) then
+		self.TeamSelectFrame:Remove()
+		self.TeamSelectFrame = nil
+	end
+
+end
---
+++ backup/init.lua
@@ -0,0 +1,176 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("cl_scoreboard.lua")
+AddCSLuaFile("cl_targetid.lua")
+AddCSLuaFile("cl_hudpickup.lua")
+AddCSLuaFile("cl_spawnmenu.lua")
+AddCSLuaFile("cl_deathnotice.lua")
+AddCSLuaFile("cl_pickteam.lua")
+AddCSLuaFile("cl_voice.lua")
+
+AddCSLuaFile("obj_player_extend.lua")
+AddCSLuaFile("gravitygun.lua")
+AddCSLuaFile("player_shd.lua")
+AddCSLuaFile("animations.lua")
+AddCSLuaFile("player_class/player_default.lua")
+AddCSLuaFile("player_class/player_survivor.lua")
+AddCSLuaFile("player_class/player_monster.lua")
+AddCSLuaFile("util.lua")
+
+-- Disabled flashlight because it's bugged.
+--AddCSLuaFile("flashlight_init.lua")
+
+include("shared.lua")
+include("player.lua")
+include("npc.lua")
+include("variable_edit.lua")
+include("resources.lua")
+
+CURRENT_ALIVE = {}
+
+GM.PlayerSpawnTime = {}
+
+--[[---------------------------------------------------------
+   Name: gamemode:Initialize()
+   Desc: Called immediately after starting the gamemode
+-----------------------------------------------------------]]
+function GM:Initialize()
+	MsgN("Initializing Horror Escape...")
+	WaitForPlayers()
+end
+
+function WaitForPlayers()
+	-- Sets required players to start a game
+	if (table.Count ( ACTIVE_PLAYERS ) >= 1) then 
+		timer.Stop("waitingforply")
+		timer.Create("preptime",5,0,function() timer.Stop("preptime") hook.Call("NEW_ROUND", GAMEMODE) end)
+ 	else
+ 		PrintMessage(HUD_PRINTCENTER, "Waiting for more players")
+ 		if not timer.Start("waitingforply") then
+ 			timer.Create("waitingforply", 2, 0, WaitForPlayers)
+ 		end
+ 	end
+end
+
+--[[---------------------------------------------------------
+   Name: gamemode:InitPostEntity()
+   Desc: Called as soon as all map entities have been spawned
+-----------------------------------------------------------]]
+function GM:InitPostEntity()
+	--hook.Call( "NEW_ROUND", GAMEMODE )
+end
+
+--[[---------------------------------------------------------
+   Name: gamemode:Think()
+   Desc: Called every frame
+-----------------------------------------------------------]]
+function GM:Think()
+end
+
+--[[---------------------------------------------------------
+   Name: gamemode:ShutDown()
+   Desc: Called when the Lua system is about to shut down
+-----------------------------------------------------------]]
+function GM:ShutDown()
+end
+
+--[[---------------------------------------------------------
+   Name: gamemode:DoPlayerDeath()
+   Desc: Carries out actions when the player dies
+-----------------------------------------------------------]]
+
+function GM:DoPlayerDeath(ply, attacker, dmginfo)
+
+	ply:CreateRagdoll()
+	local ragdoll = ply:GetRagdollEntity();
+	ragdoll:EmitSound(Sound("wilhelm.wav"));
+
+	ply:AddDeaths(1)
+
+	if (attacker:IsValid() and attacker:IsPlayer()) then
+
+		if (attacker == ply) then
+			attacker:AddFrags(-1)
+		else
+			attacker:AddFrags(1)
+		end
+
+	end
+
+	DetectEndRound();
+	table.RemoveByValue(CURRENT_ALIVE, ply)
+	if (#CURRENT_ALIVE < 2) then
+		PrintMessage(HUD_PRINTCENTER, "Game over from Rune!")
+	end
+
+end
+
+--[[---------------------------------------------------------
+   Name: gamemode:EntityTakeDamage(ent, info)
+   Desc: The entity has received damage
+-----------------------------------------------------------]]
+function GM:EntityTakeDamage(ent, info)
+end
+
+--[[---------------------------------------------------------
+   Name: gamemode:CreateEntityRagdoll(entity, ragdoll)
+   Desc: A ragdoll of an entity has been created
+-----------------------------------------------------------]]
+function GM:CreateEntityRagdoll(entity, ragdoll)
+end
+
+-- Set the ServerName every 30 seconds in case it changes..
+-- This is for backwards compatibility only - client can now use GetHostName()
+local function HostnameThink()
+
+	SetGlobalString("ServerName", GetHostName())
+
+end
+
+timer.Create("HostnameThink", 30, 0, HostnameThink)
+
+--[[---------------------------------------------------------
+	Show the default team selection screen
+-----------------------------------------------------------]]
+function GM:ShowTeam(ply)
+
+	if (not GAMEMODE.TeamBased) then return end
+	-- Disabled for dev purposes. If enabled will not allow players to change team before a timer has ended.
+	if (not GAMEMODE.DevMode) then
+		local TimeBetweenSwitches = GAMEMODE.SecondsBetweenTeamSwitches or 10
+		if (ply.LastTeamSwitch and RealTime() - ply.LastTeamSwitch < TimeBetweenSwitches) then
+			ply.LastTeamSwitch = ply.LastTeamSwitch + 1
+			ply:ChatPrint(Format("Please wait %i more seconds before trying to change team again", (TimeBetweenSwitches - (RealTime() - ply.LastTeamSwitch)) + 1))
+			return false
+		end
+	end
+	-- For clientside see cl_pickteam.lua
+	ply:SendLua("GAMEMODE:ShowTeam()")
+
+end
+
+--
+-- CheckPassword(steamid, networkid, server_password, password, name)
+--
+-- Called every time a non-localhost player joins the server. steamid is their 64bit
+-- steamid. Return false and a reason to reject their join. Return true to allow
+-- them to join.
+--
+function GM:CheckPassword(steamid, networkid, server_password, password, name)
+
+	-- The server has sv_password set
+	if (server_password ~= "") then
+
+		-- The joining clients password doesn't match sv_password
+		if (server_password ~= password) then
+			return false, "#GameUI_ServerRejectBadPassword"
+		end
+
+	end
+
+	--
+	-- Returning true means they're allowed to join the server
+	--
+	return true
+
+end
