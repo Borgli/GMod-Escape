df1704e3d3e9b13eddbb8ce896a81117d4c611ec
Skau <kskau93@gmail.com>
2016-03-16 01:18:43 +0100

    Small changes to the help menu

--- gamemode/cl_showhelp.lua
+++ gamemode/cl_showhelp.lua
@@ -1,15 +1,22 @@
 include ('shared.lua')
 
 
+
 --[Shows a help menu when you press F1]--
-
-
 function GM:ShowHelp(ply)
 
+local SCREEN_W = 1280;
+local SCREEN_H = 720;
+local X_MULTIPLIER = ScrW() / SCREEN_W
+local Y_MULTIPLIER = ScrH() / SCREEN_H
+
+
 local frame = vgui.Create( "DFrame" )
-frame:SetSize( 500, 250 )
+frame:SetTitle("Help Menu")
+frame:SetSize( 500 * X_MULTIPLIER, 250 * Y_MULTIPLIER )
 frame:Center()
 frame:MakePopup()
+
 
 local richtext = vgui.Create( "RichText", frame)
 richtext:Dock( FILL )
@@ -19,10 +26,19 @@ richtext:InsertColorChange( 255, 255, 224, 255 )
 richtext:AppendText( "Welcome to Horror Escape!\n\n" )
 
 richtext:InsertColorChange( 192, 192, 192, 255 )
-richtext:AppendText( "There will be more info here later \n\n\n\n\n\n\n\n\n\n\n\n\n" )
+richtext:AppendText( "This gamemode is still in the making.\n" )
 
+richtext:InsertColorChange( 192, 192, 192, 255 )
+richtext:AppendText( "There will be more info here in the future. \n\n\n\n\n\n\n\n\n" )
+
+richtext:InsertColorChange( 255, 255, 224, 255 )
+richtext:AppendText( "Author: Ohm\n\n\n" )
+
+--[Because why the hell not]--
 richtext:InsertColorChange( 255, 64, 64, 255 )
 richtext:AppendText( "#ServerBrowser_ESRBNotice" )
+
+
 
 end
 
